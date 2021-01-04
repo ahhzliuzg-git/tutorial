22c0756defe1f91b29a8f94f293d44fd15a81286
ahhzliuzg <1169989199@qq.com>
2021-01-04 15:28:22 +0800

    for mutlifile downlaod

--- spiders/quotes_spider.py
+++ spiders/quotes_spider.py
@@ -4,6 +4,8 @@ from scrapy.http import Request
 import time
 import random
 from tutorial.items import TutorialItem
+from prepare_urls import yieldpage
+
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     
@@ -12,15 +14,29 @@ class QuotesSpider(scrapy.Spider):
         'USER_AGENT': 'Mozilla/5.0'
       #   'SOME_SETTING': 'some value',
     }
-    def start_requests(self):
+    def start_requests1(self):
         urls = [
 'http://search.ccgp.gov.cn/bxsearch?searchtype=2&page_index=100&bidSort=0&buyerName=&projectId=&pinMu=0&bidType=0&dbselect=bidx&kw=&start_time=2019%3A02%3A13&end_time=2019%3A08%3A14&timeType=5&displayZone=&zoneId=&pppStatus=0&agentName='
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self, response):
+    def start_requests(self):
+# search result page list ,about ten thounds page
+            test1=yieldpage(10)
 
+            while True:
+                try:
+                    #print(next(test1))
+                    time.sleep(random.uniform(1.1,5.4))
+                    url1=next(test1)
+                    yield scrapy.Request(url=url1, callback=self.parse)
+                   
+                except StopIteration:
+                    break 
+
+    def parse(self, response):
+#in  each  search result page ,we get down it's content with file form,and downlaod file attachment .
         le=LinkExtractor(restrict_xpaths='///html/body/div[5]/div[2]/div/div/div[1]/ul',deny='/index.html$')
         for link in le.extract_links(response):
             time.sleep(random.uniform(1.1,5.4))
